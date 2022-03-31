a483ae12f65f015efcb6c2b68e0e63c0c95df832
26350001 <26350001@L815WD220016.edu.cbe.ab.ca>
2022-03-31 15:05:19 -0600

    3:04

---
+++ Chapter3/src/Exercises/Ex1.java
@@ -0,0 +1,23 @@
+package Exercises;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		System.out.println("Mr. Wilson");
+		
+		System.out.println("New Computer room 314 beside the exit door");
+		
+		System.out.println("March 31, 2022");
+		
+		
+		
+		
+		System.out.println("To: Jujhar Singh");
+		
+		
+		
+		System.out.println("Help! I'm Trapped inside a computer!");
+
+	}// Updating: 2:51
+
+}
