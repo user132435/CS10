0d5f6a1828d42272a53169e742b276ff264e7464
26350001 <26350001@L815WD220016.edu.cbe.ab.ca>
2022-06-07 14:21:43 -0600

    2:21 pm

--- Chapter3/src/Assignments/Finalproject.java
+++ Chapter3/src/Assignments/Finalproject.java
@@ -5,8 +5,21 @@ import java.util.Scanner;
 public class Finalproject {
 
 	public static void main(String[] args) {
-		    {
-		    // POV you are at a restaurant and want to order something but don't know if you can afford it//
+{
+	/*
+
+	Program:Finalproject.java          Date: June 7, 2022
+
+
+	Author: Jujhar Singh 
+	School: CHHS
+	Course: Computer Science 10
+	 
+
+	*/
+	
+	
+	// POV you are at a restaurant and want to order something but don't know if you can afford it//
 		    	
 		    	
 		    	Scanner userinput = new Scanner(System.in);
@@ -52,6 +65,15 @@ public class Finalproject {
 
 		}
 	/* Screen Dump
+	Please enter your money amount or type -1 to exit the program: 
+	149
+	You can order it
+	Enter another money amount, or type -1 to leave:
+	68
+	Sorry, you can not afford it or don't have enough money
+	Enter another money amount, or type -1 to leave:
+	-1
+	Thank you for using our program, have a great day!
 
 
 
